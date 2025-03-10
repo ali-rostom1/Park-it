87245ffdb7307251de6ad2a2f33ecdaa7ef9b748
ali-rostom1 <ali.rostom220@gmail.com>
2025-03-10 13:54:10 +0000

    finish authentification using sanctum and set up parking & spots tables

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        try{
+            User::create($request->validated());
+
+        return response()->json([
+            'message' => 'User registered successfully'
+        ],201);
+        }catch(\Exception $e){
+            return response()->json([
+                'message' => 'Failed to register User',
+                'error' => $e->getMessage(),
+            ],500);
+        }
+    }
+    public function login(LoginRequest $request)
+    {
+        try{
+            $validated = $request->validated();
+            $user = User::where('email',$validated['email'])->first();
+            if(!$user || !Hash::check($validated['password'],$user->password)){
+                return response()->json([
+                    'message' => 'Invalid credentials'
+                ],401);
+            }
+            $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
+            return response()->json([
+                'access_token' => $token
+            ],201);
+        }catch(\Exception $e){
+            return response()->json([
+                'message' => 'Inputs dont match the rules'
+            ]);
+        }
+    }
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'Logged out'
+        ]);
+    }
+}
---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+    public function prepareForValidation()
+    {
+        $this->merge(['password' => Hash::make($this->get('password'))]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|string',
+            'name' => 'required|string|max:255',
+            'password' => 'required|min:8|max:255',
+        ];
+    }
+}
